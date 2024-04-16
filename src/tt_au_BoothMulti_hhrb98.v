300cd470b8c16ee131102d3ae661c9600a83694f
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-16 00:55:21 -0600

    Update tt_au_BoothMulti_hhrb98.v

--- src/tt_au_BoothMulti_hhrb98.v
+++ src/tt_au_BoothMulti_hhrb98.v
@@ -1,16 +1,18 @@
-module tt_au_BoothMulti_hhrb98(X, Y, Z,uio_out, uio_oe,clk,rst_n, ena);
+module tt_au_BoothMulti_hhrb98(
  output wire [7:0] uio_out,  // IOs: Output path
  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)  input  wire       ena,      // will go high when the design is enabled
  input  wire       clk,      // clock
- input  wire       rst_n     // reset_n - low to reset
- input  wire       ena,      // will go high when the design is enabled
- input [3:0] X, Y;
- output [7:0] Z;
- reg [7:0] Z;
- reg [3:0] temp;
- integer i;
- reg E1;
- reg [3:0] Y1;
+ input  wire       rst_n,     // reset_n - low to reset
+ input  wire       ena      // will go high when the design is enabled
+ input [3:0] X, Y,
+ output [7:0] Z,
+ reg [7:0] Z,
+ reg [3:0] temp,
+ integer i,
+ reg E1,
+ reg [3:0] Y1
+ );
+
  always @ (X, Y)
  begin
  Z = 8'd0;
