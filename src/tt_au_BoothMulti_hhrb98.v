bd2854486b0c7e6cb606329b10dc47eb24f8025a
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-16 00:57:23 -0600

    Update tt_au_BoothMulti_hhrb98.v

--- src/tt_au_BoothMulti_hhrb98.v
+++ src/tt_au_BoothMulti_hhrb98.v
@@ -1,11 +1,11 @@
-module tt_au_BoothMulti_hhrb98(
+module tt_au_BoothMulti_hhrb98 (
  output wire [7:0] uio_out,  // IOs: Output path
  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)  input  wire       ena,      // will go high when the design is enabled
  input  wire       clk,      // clock
  input  wire       rst_n,     // reset_n - low to reset
- input  wire       ena      // will go high when the design is enabled
- input [3:0] X, Y,
- output [7:0] Z,
+ input  wire       ena,      // will go high when the design is enabled
+ input wire [3:0] X, Y,
+ output wire [7:0] Z,
  reg [7:0] Z,
  reg [3:0] temp,
  integer i,
