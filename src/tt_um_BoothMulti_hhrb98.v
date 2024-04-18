072b84a9560902884686bdf370c29c5231909fc4
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 11:44:55 -0600

    Update tt_um_BoothMulti_hhrb98.v

--- src/tt_um_BoothMulti_hhrb98.v
+++ src/tt_um_BoothMulti_hhrb98.v
@@ -1,4 +1,4 @@
-module BoothMulti_4bit(
+module tt_um_BoothMulti_hhrb98(
     input  wire [7:0] ui_in,     // Dedicated inputs
     output wire [7:0] uo_out,    // Dedicated outputs
     input  wire [7:0] uio_in,    // IOs: Input path
