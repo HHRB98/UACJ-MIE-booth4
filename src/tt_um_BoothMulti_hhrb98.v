99d31c9e55e1cdddb1a2cd69400725870f6f434e
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 13:01:19 -0600

    Update tt_um_BoothMulti_hhrb98.v

--- src/tt_um_BoothMulti_hhrb98.v
+++ src/tt_um_BoothMulti_hhrb98.v
@@ -1,12 +1,12 @@
 module tt_um_BoothMulti_hhrb98(
-    input  wire [7:0] ui_in,     // Dedicated inputs
-    output wire [7:0] uo_out,    // Dedicated outputs
-    input  wire [7:0] uio_in,    // IOs: Input path
-    output wire [7:0] uio_out,   // IOs: Output path
-    output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
-    input wire        clk,
-    input  wire       ena,       // will go high when the design is enabled
-    input  wire       rst_n      // reset_n - low to reset
+  input  wire [7:0] ui_in,     // Dedicated inputs
+  output wire [7:0] uo_out,    // Dedicated outputs
+  input  wire [7:0] uio_in,    // IOs: Input path
+  output wire [7:0] uio_out,   // IOs: Output path
+  output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
+  input wire        clk,
+  input  wire       ena,       // will go high when the design is enabled
+  input  wire       rst_n      // reset_n - low to reset
 );
 
   // Inputs wire
@@ -17,7 +17,7 @@ module tt_um_BoothMulti_hhrb98(
 
   // Assigning values to output wires
   assign uio_out = Z;
-  assign uio_oe = 8'b11111111;
+  assign uio_oe = ~rst_n;
 
   // Extracting bits from input
   assign X = ui_in[3:0];
@@ -38,7 +38,7 @@ module tt_um_BoothMulti_hhrb98(
       case (temp)
         2'b10: Z1 = Z1 + Y;
         2'b01: Z1 = Z1 - Y;
-        default: begin end
+        default: Z1 = Z1; // Added default case to avoid latches
       endcase
       Z1 = Z1 >> 1;
       E1 = X[i];
