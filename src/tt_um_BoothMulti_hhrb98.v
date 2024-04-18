dfe57993fefb687faa058bde451924b7ac311ec6
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 10:23:32 -0600

    Update tt_um_BoothMulti_hhrb98.v

--- src/tt_um_BoothMulti_hhrb98.v
+++ src/tt_um_BoothMulti_hhrb98.v
@@ -1,22 +1,22 @@
 module tt_um_BoothMulti_hhrb98(
-    input  wire [7:0] ui_in,     // Dedicated inputs
-    output wire [7:0] uo_out,    // Dedicated outputs
-    input  wire [7:0] uio_in,    // IOs: Input path
-    output wire [7:0] uio_out,   // IOs: Output path
-    output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
-    input wire        clk,
-    input  wire       ena,       // will go high when the design is enabled
-    input  wire       rst_n      // reset_n - low to reset
+  input  wire [7:0] ui_in,     // Dedicated inputs
+  output wire [7:0] uo_out,    // Dedicated outputs
+  input  wire [7:0] uio_in,    // IOs: Input path
+  output wire [7:0] uio_out,   // IOs: Output path
+  output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
+  input wire        clk,
+  input  wire       ena,       // will go high when the design is enabled
+  input  wire       rst_n      // reset_n - low to reset
 );
 
   // Inputs wire
   wire [3:0] X, Y;
 
   // Output wire
-  wire signed [7:0] Z;
+  wire [7:0] Z;
 
   // Assigning values to output wires
-  assign uio_out = 8'b11111111;
+  assign uio_out = Z;
   assign uio_oe = 8'b11111111;
   
   // Extracting bits from input
@@ -36,11 +36,11 @@ module tt_um_BoothMulti_hhrb98(
     end
   end
 
-  reg signed [7:0] Z1;
+  reg [7:0] Z1;
   reg [3:0] temp;
   integer i;
   reg E1;
-  reg signed [3:0] Y1;
+  reg [3:0] Y1;
 
   always @ (X, Y)
   begin
@@ -49,25 +49,17 @@ module tt_um_BoothMulti_hhrb98(
     for (i = 0; i < 4; i = i + 1)
     begin
       temp = {X[i], E1};
-      if (Y[3] == 1'b1)
-        Y1 = -Y;
-      else
-        Y1 = Y;
+      Y1 = Y;
       case (temp)
         2'b10: Z1[7:4] = Z1[7:4] + Y1[3:0];
         2'b01: Z1[7:4] = Z1[7:4] + Y[3:0];
         default: begin end
       endcase
       Z1 = Z1 >> 1;
-      Z1[7] = Z1[6];
       E1 = X[i];
-    end
-    if (Y == 4'd8)
-    begin
-      Z1 = -Z1;
     end
   end
 
-  assign uo_out[7:0] = Z1[7:0];
+  assign uo_out = Z1;
 
 endmodule
