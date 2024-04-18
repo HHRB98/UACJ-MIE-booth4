9388777c068471cd9fd020e140ab86322ed2c3f5
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 12:53:48 -0600

    Update tt_um_BoothMulti_hhrb98.v

--- src/tt_um_BoothMulti_hhrb98.v
+++ src/tt_um_BoothMulti_hhrb98.v
@@ -18,44 +18,26 @@ module tt_um_BoothMulti_hhrb98(
   // Assigning values to output wires
   assign uio_out = Z;
   assign uio_oe = 8'b11111111;
-  
+
   // Extracting bits from input
   assign X = ui_in[3:0];
   assign Y = ui_in[7:4];
-
-  // Flip flop
-  reg variable; // Changed to reg type
-
-  always @(posedge clk or negedge rst_n) begin  
-    if (~rst_n) begin
-      // Reset condition: set variable to 0
-      variable <= 1'b0; // Changed value to 1-bit
-    end else begin
-      // Update variable with a value
-      variable <= ena; // Assigning ena to variable
-    end
-  end
 
   reg [7:0] Z1;
   reg [3:0] temp;
   integer i;
   reg E1;
-  reg [3:0] Y1;
 
   always @ (X, Y)
   begin
     Z1 = 8'd0;
-    E1 = 1'd0;
+    E1 = 1'b0;
     for (i = 0; i < 4; i = i + 1)
     begin
       temp = {X[i], E1};
-      if (Y[3] == 1'b1)
-        Y1 = -Y;
-      else
-        Y1 = Y;
       case (temp)
-        2'b10: Z1 = Z1 + Y1;
-        2'b01: Z1 = Z1 + Y;
+        2'b10: Z1 = Z1 + Y;
+        2'b01: Z1 = Z1 - Y;
         default: begin end
       endcase
       Z1 = Z1 >> 1;
