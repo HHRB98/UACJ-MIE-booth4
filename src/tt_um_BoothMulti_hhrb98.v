c7c581a4b8c7d7dbc65ed0597b7a61da64134b36
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 13:58:42 -0600

    Update tt_um_BoothMulti_hhrb98.v

--- src/tt_um_BoothMulti_hhrb98.v
+++ src/tt_um_BoothMulti_hhrb98.v
@@ -1,8 +1,8 @@
 module tt_um_BoothMulti_hhrb98(
-  input  wire [7:0] ui_in,     // Dedicated inputs
-  output wire [7:0] uo_out,    // Dedicated outputs
-  input  wire [7:0] uio_in,    // IOs: Input path
-  output wire [7:0] uio_out,   // IOs: Output path
+  input  wire [3:0] ui_in,     // Dedicated inputs (restricted to positive numbers)
+  output wire [7:0] uo_out,    // Dedicated outputs (positive numbers only)
+  input  wire [7:0] uio_in,    // IOs: Input path (restricted to positive numbers)
+  output wire [7:0] uio_out,   // IOs: Output path (positive numbers only)
   output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
   input wire        clk,
   input  wire       ena,       // will go high when the design is enabled
@@ -10,26 +10,27 @@ module tt_um_BoothMulti_hhrb98(
 );
 
   // Inputs wire
-  wire [3:0] X, Y;
+  wire [3:0] X;
+  wire [3:0] Y;
 
   // Output wire
-  reg [7:0] Z;
+  wire [7:0] Z;
 
   // Assigning values to output wires
   assign uio_out = Z;
   assign uio_oe = ~rst_n;
 
   // Extracting bits from input
-  assign X = ui_in[3:0];
-  assign Y = ui_in[7:4];
-
-  reg [7:0] Z1;
-  reg [3:0] temp;
-  integer i;
-  reg E1;
+  assign X = ui_in;
+  assign Y = uio_in[7:4];
 
   always @ (X, Y)
   begin
+    reg [7:0] Z1;
+    reg [3:0] temp;
+    integer i;
+    reg E1;
+
     Z1 = 8'd0;
     E1 = 1'b0;
     for (i = 0; i < 4; i = i + 1)
@@ -42,7 +43,10 @@ module tt_um_BoothMulti_hhrb98(
       endcase
       E1 = X[i];
     end
-    Z = Z1; // Assigning Z1 to Z
+    if (Z1[7] == 1) // If the MSB of Z1 is 1, set Z to 0
+      Z = 8'd0;
+    else
+      Z = Z1; // Assign Z1 to Z
   end
 
   assign uo_out = Z;
