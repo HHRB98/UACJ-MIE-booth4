79e89c1fe9459416748573c56e27f7028d87fcb2
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-18 01:30:43 -0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -24,9 +24,9 @@ async def test_project(dut):
 
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 100
+  dut.ui_in.value = 132
   dut.uio_in.value = 0
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 24
+  assert dut.uo_out.value == 32
